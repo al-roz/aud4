389d873fec82f022474c75c494f16eaad18f7baa
Roz <sasha_royz@mail.ru>
2020-03-17 17:20:32 +0500

    Add func pop and push

--- Aud4/main.c
+++ Aud4/main.c
@@ -4,12 +4,33 @@
 typedef struct
 {
 	double value;
-	List* next;
-}List;
+	struct Node* next;
+}Node;
+
+//добавить элемент в список
+void push(Node** head, int data) {
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->value = data;
+	tmp->next = (*head);
+	(*head) = tmp;
+}
+
+//удалить элемент из списка
+int pop(Node** head) {
+    Node* prev = NULL;
+    int val;
+    if (head == NULL) {
+        exit(-1);
+    }
+    prev = (*head);
+    val = prev->value;
+    (*head) = (*head)->next;
+    free(prev);
+    return val;
+}
 
 int main()
 {
-
-
-
+	srand(time(NULL));
+	Node* head = NULL;
 }
